eb80fcc1935a92c387a5d069f7dcfb89e9e5f2b0
AnastasiyaVetrova <Vetrova.Java.Dev@gmail.com>
2024-06-15 19:07:38 +0300

    Create application.yaml

--- src/main/java/com/aston/aston_project/AstonProjectApplication.java
+++ src/main/java/com/aston/aston_project/AstonProjectApplication.java
@@ -1,13 +1,17 @@
 package com.aston.aston_project;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AstonProjectApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(AstonProjectApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(AstonProjectApplication.class, args);
+        log.info("Приложение запустилось");
     }
-
 }
