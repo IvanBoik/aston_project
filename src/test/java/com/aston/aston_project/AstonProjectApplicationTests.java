183a83dc0a5e98fb4f309373f3130a07de32e1a2
IvanBoik <boiko12332@gmail.com>
2024-06-15 15:51:29 +0400

    Init commit

---
+++ src/test/java/com/aston/aston_project/AstonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aston.aston_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstonProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
