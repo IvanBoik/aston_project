0a9f5f231b7a278f6f39caf0c47c4d6c9d33850d
Anastasiya Pomogaeva <pomogaeva.anasyasiya@gmail.com>
2024-06-17 16:35:13 +0300

    product class expanded, product-type, producer and country added

---
+++ src/main/java/com/aston/aston_project/entity/Country.java
@@ -0,0 +1,21 @@
+package com.aston.aston_project.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
--- src/main/java/com/aston/aston_project/entity/Product.java
+++ src/main/java/com/aston/aston_project/entity/Product.java
@@ -13,14 +13,28 @@ import java.util.Map;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
+
     private String name;
+
+    private Float price;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id")
+    private Type type;
+
+    @Column(name = "recepie")
+    private Boolean isPrescriptionRequired;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id")
+    private Producer producer;
+
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "attribute_value",
             joinColumns = @JoinColumn(name = "product_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "value_id", referencedColumnName = "id")
     )
-    @MapKeyJoinColumn (name = "attribute_id",referencedColumnName = "id")
+    @MapKeyJoinColumn(name = "attribute_id", referencedColumnName = "id")
     private Map<Attribute, Value> attributesValues = new HashMap<>();
-
 }
