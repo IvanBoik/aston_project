61b666270ba791e7e3a768c1297ce595db6b36db
AnastasiyaVetrova <Vetrova.Java.Dev@gmail.com>
2024-07-04 01:07:43 +0300

    Controller Test

--- src/main/java/com/aston/aston_project/AstonProjectApplication.java
+++ src/main/java/com/aston/aston_project/AstonProjectApplication.java
@@ -3,9 +3,7 @@ package com.aston.aston_project;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -18,5 +16,6 @@ public class AstonProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(AstonProjectApplication.class, args);
         log.info("Приложение запустилось");
+
     }
 }
